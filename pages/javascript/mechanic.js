c4ee454a9d47d8c9aec838848b67bf0cf7f622fb
razak2004 <abdulrazak.nasriudeen@fssa.freshworks.com>
2023-03-29 00:01:02 +0530

    build: added_mechanic_page

--- pages/javascript/mechanic.js
+++ pages/javascript/mechanic.js
@@ -29,3 +29,19 @@ function openProServices(id) {
   }
   document.getElementById(id).style.display = "flex";
 }
+
+// function for token
+
+const token = document.querySelector(".token");
+token.addEventListener("mouseover", (e) => {
+  e = document.querySelector(".tokenDesc");
+  e.style.display = "flex";
+});
+
+const tokendesc = document.querySelector(".tokenDesc");
+tokendesc.addEventListener("mouseover", (e) => {
+  tokendesc.style.display = "flex";
+});
+tokendesc.addEventListener("mouseout", (e) => {
+  tokendesc.style.display = "none";
+});
