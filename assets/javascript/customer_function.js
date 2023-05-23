e1447517348837121a9f3287c3e5139fd5056657
razak2004 <abdulrazak.nasriudeen@fssa.freshworks.com>
2023-05-23 19:57:57 +0530

    build: added_filter

--- assets/javascript/customer_function.js
+++ assets/javascript/customer_function.js
@@ -1,4 +1,4 @@
-const loggedUser = localStorage.getItem("LogUser");
+const loggedUser = localStorage.getItem("LoginUser");
 let users = JSON.parse(localStorage.getItem("users"));
 let loggedUserObj = users.find((e) => {
   if (e["user_id"] == loggedUser) {
@@ -8,3 +8,54 @@ let loggedUserObj = users.find((e) => {
 
 let showName = document.getElementById("userName");
 showName.innerText = "Hi..!      " + loggedUserObj["name"];
+let workShops = JSON.parse(localStorage.getItem("workshops"));
+
+//
+for (let i = 0; i < workShops.length; i++) {
+  createWorkshop(workShops[i], "workshops");
+}
+// filter function
+function filter(arr, val, id) {
+  let fliterArr = arr.filter((e) => {
+    if (e["workshopType"] == val) {
+      return true;
+    }
+  });
+  let section = document.getElementById(id);
+  while (section.hasChildNodes()) {
+    // remove existing child elements
+    section.firstChild.remove();
+  }
+  for (let i = 0; i < fliterArr.length; i++) {
+    createWorkshop(fliterArr[i], id);
+  }
+}
+
+let filterTwoWheeler = document.getElementById("twoWheeler");
+filterTwoWheeler.addEventListener("click", () => {
+  filter(workShops, "2 wheelers", "workshops");
+});
+let filterThreeWheeler = document.getElementById("threeWheeler");
+filterThreeWheeler.addEventListener("click", () => {
+  filter(workShops, "3 wheelers", "workshops");
+});
+let filterFourWheeler = document.getElementById("fourWheeler");
+filterFourWheeler.addEventListener("click", () => {
+  filter(workShops, "4 wheelers", "workshops");
+});
+
+// search
+const searchbar = document.getElementById("searchbar");
+const cards = document.getElementsByClassName("workShopCard");
+
+searchbar.addEventListener("input", () => {
+  for (const element of cards) {
+    if (
+      element.innerHTML.toLowerCase().includes(searchbar.value.toLowerCase())
+    ) {
+      element.style.display = "flex";
+    } else {
+      element.style.display = "none";
+    }
+  }
+});
