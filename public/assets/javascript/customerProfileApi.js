ff2539febdb61db005460ee4204414098cdf8929
razak2004 <abdulrazak.nasriudeen@fssa.freshworks.com>
2023-10-21 14:20:17 +0530

    deleted previous version

--- public/assets/javascript/customerProfileApi.js
+++
@@ -1,4 +0,0 @@
-const profileName = document.getElementById("profileName");
-const profileNumber = document.getElementById("profileNumber");
-profileName.innerText = user.name;
-profileNumber.innerText = user.number;
